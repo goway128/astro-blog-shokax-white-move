bb3d8bc0d4c1a9e7066d3c37d140b5528e45d389
goway128 <goway0128@outlook.com>
2026-08-20 22:18:42 +0800

    更新友链

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -118,6 +118,14 @@ export default defineConfig({
         description: "认识的朋友们，欢迎互访。",
         links: [
           {
+            url: "https://www.10dianai.com",
+            title: "10dian-AI",
+            desc: "AI、二次元、游戏、各种你喜欢的",
+            author: "10dian",
+            avatar: "http://img.10dianai.com/10dianai-w.jpg",
+            color: "var(--color-blue)",
+          },         
+          {
             url: "https://mahiro.uk/",
             title: "Smirnova Oyama",
             desc: "一个本科计算机学生",
